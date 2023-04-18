e1a5b695ed4b53d12a3f414aa0b655e5291c18be
Joosep Parts <joosep.parts@gmail.com>
2023-04-18 08:41:23 +0300

    octo
    (cherry picked from commit 5cf02a2c27fc258c576a1ba23684797f00f5e939)

--- homeworks/04.21/octopawn.py
+++ homeworks/04.21/octopawn.py
@@ -1,42 +1,30 @@
-def read_settings():
-    global central_bonus, protection_bonus, capturing_bonus, mobility_bonus, potential_capture_bonus, exposure_penalty, aggressive_capture_bonus
+# Prioritize:
+# 1. Force enemy into bad move
+# 2. Take enemy pawn
+# 3. Make empty move when no other options
+# 4. Protect diagonal vectors
+# 5. Try to control center (to take pawns)
+# 6. Alpha-beta pruning (search through a larger space of possible moves)
 
-central_bonus = 3
-protection_bonus = 8
-capturing_bonus = 8
-mobility_bonus = 2
-potential_capture_bonus = 6
-exposure_penalty = 10
-aggressive_capture_bonus = 1
+# Version 1.0 is 14.55% win rate
+# Version 1.1 is 88.00% win rate
+# Version 1.2 ???
 
-try:
-    with open("settings.txt", "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            key, value = line.strip().split('=')
-            if key == "central_bonus":
-                central_bonus = int(value)
-            elif key == "protection_bonus":
-                protection_bonus = int(value)
-            elif key == "capturing_bonus":
-                capturing_bonus = int(value)
-            elif key == "mobility_bonus":
-                mobility_bonus = int(value)
-            elif key == "potential_capture_bonus":
-                potential_capture_bonus = int(value)
-            elif key == "exposure_penalty":
-                exposure_penalty = int(value)
-            elif key == "aggressive_capture_bonus":
-                aggressive_capture_bonus = int(value)
-except FileNotFoundError:
-    print("Settings file not found. Using default values.")
-
+# Completed games: 1000
+# ----------------------Wins statistics:----------------------
+# White (Player 1): 880 (88.00%)
+# Black (Player 2): 120 (12.00%)
+# Draws: 0 (0.00%)
+# -----------------Win statistics bar chart:------------------
+# Player 1: ################################################## (880, 88.00%)
+# Player 2: ###### (120, 12.00%)
+# Draws:  (0, 0.00%)
+# ------------------------------------------------------------
 
 def find_move(board, player):
     empty_moves = []
     capture_moves = []
     forced_capture_moves = []
-    corner_moves = []
 
     for row in range(len(board)):
         for col in range(len(board[row])):
@@ -53,24 +41,18 @@ def find_move(board, player):
                             forced_capture_moves.append(move)
                     else:
                         empty_moves.append(move)
-                        if move[1] == (0, 3) and player == 1 or move[1] == (3, 0) and player == 2:
-                            corner_moves.append(move)
 
     capture_moves.sort(key=lambda move: move[1][0], reverse=(player == 1))
     empty_moves.sort(key=lambda move: (evaluate_move(board, player, move), move[1][0]), reverse=(player == 1))
-    corner_moves.sort(key=lambda move: move[1][0], reverse=(player == 1))
 
     if forced_capture_moves:
         return forced_capture_moves[0]
     elif capture_moves:
         return capture_moves[0]
-    elif corner_moves:
-        return corner_moves[0]
     elif empty_moves:
         return empty_moves[0]
     else:
         return None
-
 
 def get_all_valid_moves(board, player):
     all_moves = []
@@ -81,14 +63,12 @@ def get_all_valid_moves(board, player):
                 all_moves.extend(moves)
     return all_moves
 
-
 def make_move(board, player, move):
     start, end = move
     start_row, start_col = start
     end_row, end_col = end
     board[end_row][end_col] = player
     board[start_row][start_col] = 0
-
 
 def get_valid_moves(board, player, row, col):
     direction = 1 if player == 1 else -1
@@ -103,12 +83,10 @@ def get_valid_moves(board, player, row, col):
 
     return valid_moves
 
-
 def is_capture_move(board, move):
     start, end = move
     end_row, end_col = end
     return board[end_row][end_col] != 0
-
 
 def evaluate_move(board, player, move):
     start, end = move
@@ -116,44 +94,13 @@ def evaluate_move(board, player, move):
     end_row, end_col = end
 
     # Center control
-    a = 0
-    if end_col in [1, 2]:
-        a = central_bonus
+    central_bonus = 0
+    if end_col in [2, 3]:
+        central_bonus = 1
 
     # Diagonal protection
-    b = 0
-    if (end_col - 1 >= 0 and board[end_row][end_col - 1] == player) or (
-            end_col + 1 < len(board[end_row]) and board[end_row][end_col + 1] == player):
-        b = 3
+    protection_bonus = 0
+    if (end_col - 1 >= 0 and board[end_row][end_col - 1] == player) or (end_col + 1 < len(board[end_row]) and board[end_row][end_col + 1] == player):
+        protection_bonus = 1
 
-    # Capturing bonus
-    d = 0
-    if is_capture_move(board, move):
-        d = capturing_bonus
-
-    # Exposure penalty
-    e = 0
-    temp_board = [row[:] for row in board]
-    make_move(temp_board, player, move)
-    opponent_moves = get_all_valid_moves(temp_board, 3 - player)
-    if any(is_capture_move(temp_board, m) and m[1] == end for m in opponent_moves):
-        e = exposure_penalty
-
-    # Aggressive capture bonus
-    f = 0
-    temp_board = [row[:] for row in board]
-    make_move(temp_board, player, move)
-    opponent_pieces = sum(1 for row in temp_board for cell in row if cell == 3 - player)
-    if opponent_pieces <= 2:
-        f = aggressive_capture_bonus
-
-    # Mobility
-    temp_board = [row[:] for row in board]
-    make_move(temp_board, player, move)
-    g = mobility_bonus * len(get_all_valid_moves(temp_board, player))
-
-    # Potential captures
-    potential_captures = sum(1 for m in get_all_valid_moves(temp_board, player) if is_capture_move(temp_board, m))
-    h = potential_capture_bonus * potential_captures * 2
-
-    return a + b + d - e + f + g + h
+    return central_bonus + protection_bonus
--- homeworks/04.21/simulation.py
+++ homeworks/04.21/simulation.py
@@ -1,4 +1,3 @@
-import itertools
 import sys
 import subprocess
 from collections import defaultdict
@@ -12,71 +11,16 @@ def clear_console():
     else:
         os.system("clear")
 
-def load_settings():
-    with open("settings.txt", "r") as f:
-        lines = [line.strip() for line in f.readlines()]
-        settings = {}
-        for line in lines:
-            key, value = line.split(" = ")
-            settings[key] = int(value)
-        return settings
-
-def update_settings_file(settings):
-    with open("settings.txt", "w") as f:
-        for key, value in settings.items():
-            f.write(f"{key} = {value}\n")
-
-import random
-
-def update_settings(settings):
-    for key, value in settings.items():
-        new_value = random.randint(1, 10)
-        settings[key] = new_value
-    update_settings_file(settings)
-
-
-
-def load_sim_stats():
-    if not os.path.exists("sim_stats.txt"):
-        return {}
-
-    with open("sim_stats.txt", "r") as f:
-        try:
-            data = json.load(f)
-        except json.JSONDecodeError:
-            data = {}
-    return data
-
-def save_sim_stats(data):
-    with open("sim_stats.txt", "w") as f:
-        json.dump(data, f, indent=2)
-
 def main():
-    num_simulations = 10000
-    print_interval = 200
-    num_games_per_config = 200
-    update_interval = num_games_per_config
+    num_simulations = 100
+    print_interval = 1
 
-    settings = load_settings()
-    sim_stats = load_sim_stats()
+    data = load_data()
+    wins = data["wins"]
+    my_wins = data["my_wins"]
+    AI_wins = data["AI_wins"]
 
-    for i in range(1, num_simulations + 1):
-        if i % update_interval == 1:
-            update_settings(settings)
-            print(settings)
-
-
-        config_key = json.dumps(settings, sort_keys=True)
-
-        if config_key not in sim_stats:
-            sim_stats[config_key] = {
-                "wins": defaultdict(int, {1: 0, 2: 0, 0: 0}),
-                "my_wins": [],
-                "AI_wins": [],
-            }
-
-        config_data = sim_stats[config_key]
-
+    for i in range(len(my_wins) + len(AI_wins) + 1, len(my_wins) + len(AI_wins) + num_simulations + 1):
         result = subprocess.run([sys.executable, "game.py"], capture_output=True)
         output = result.stdout.decode('utf-8').strip()
 
@@ -90,20 +34,20 @@ def main():
         winner_line = winner_lines[0]
         winner = int(winner_line.split()[1])
 
-        config_data["wins"][winner] += 1
+        wins[winner] += 1
         moves = ' '.join(lines[:-2])
 
         if winner == 1:
-            config_data["my_wins"].append(moves)
+            my_wins.append(moves)
         elif winner == 2:
-            config_data["AI_wins"].append(moves)
+            AI_wins.append(moves)
 
-        save_sim_stats(sim_stats)
+        save_to_file(wins, my_wins, AI_wins)
 
         if i % print_interval == 0:
             clear_console()
             print(f"Completed games: {i}")
-            print_statistics(config_data["wins"])
+            print_statistics(wins)
 
 def print_statistics(wins):
     total_games = sum(wins.values())
